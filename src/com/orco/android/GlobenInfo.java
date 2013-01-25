fef3b877944d2c52f182eab2ddcc609caf6aa8d2
Christian Olsson <Christian.Olsson@gmail.com>
2013-01-25 21:05:27 +0100

    Nearly working

--- src/com/orco/android/GlobenInfo.java
+++ src/com/orco/android/GlobenInfo.java
@@ -11,11 +11,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class GlobenInfo extends Activity {
     private WebView browser;
     private String username;
     private String password;
+    private Boolean automaticallyLogin;
+    private final String SchoolSoftUrl = "https://sms11.schoolsoft.se/globen/jsp/pda/Login.jsp";
+    private Integer loginAttemptsLeft = 1;
+    private String lastUrl = "";
+    private Boolean tryingToLogin = false;
 
     /** Called when the activity is first created. */
     @SuppressLint("SetJavaScriptEnabled")
@@ -26,42 +32,79 @@ public class GlobenInfo extends Activity {
         browser=(WebView)findViewById(R.id.webkit);
         browser.getSettings().setJavaScriptEnabled(true);
         loadPrefs();
+        injectJavaScript();
+        loadSchoolsoft();
+    }
 
+
+    private void injectJavaScript() {
         /* WebViewClient must be set BEFORE calling loadUrl! */  
         browser.setWebViewClient(new WebViewClient() {  
             @Override  
-            public void onPageFinished(WebView view, String url)  
-            {  
-                try
-                {     
-                    String jsUrlStart = "javascript:(function() {document.";
-                    String jsUrlEnd = "; })()";
-                    browser.loadUrl(jsUrlStart
-                            + "getElementById('usertype').value = '2'"
-                            + jsUrlEnd);
-                    browser.loadUrl(jsUrlStart
-                            + "getElementsByName('ssusername')[0].value = '"
-                            + username + "'" + jsUrlEnd);
-                    browser.loadUrl(jsUrlStart
-                            + "getElementsByName('sspassword')[0].value = '"
-                            + password + "'" + jsUrlEnd);
-                    // browser.loadUrl("javascript:(function() {document.forms['userForm'].submit(); })()");
+            public void onPageFinished(WebView view, String url) {
+                final Boolean onLoginPage   = url.equals(SchoolSoftUrl);
+                final Boolean lastWasLogin  = lastUrl.equals(SchoolSoftUrl);
+                final Boolean loginSucceded = !onLoginPage && lastWasLogin;
+                lastUrl = url;
+
+
+                /* If we are on another page and not just logged in, just do nothing*/
+                if (!onLoginPage && !lastWasLogin) {
+                    return;
                 }
-                catch(Exception ex)
-                {
-                    Log.e("Error : " , "Error in onPageFinished() " + ex.getMessage());
-                    ex.printStackTrace();
+
+                if (loginSucceded) {
+                    Toast.makeText(getApplicationContext(), "Inloggad!",
+                            Toast.LENGTH_SHORT).show();
+                    loginAttemptsLeft = 1;
+                    return;
+                }
+
+                if (onLoginPage) {
+                    if (tryingToLogin) {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Misslyckades logga in, kontrollera användarnamn och lösenord!",
+                                Toast.LENGTH_LONG).show();
+                        tryingToLogin = false;
+                        startPrefActivity();
+                    } else {
+                        try {
+                            String jsUrlStart = "javascript:(function() {document.";
+                            String jsUrlEnd = "; })()";
+                            browser.loadUrl(jsUrlStart
+                                    + "getElementById('usertype').value = '2'"
+                                    + jsUrlEnd);
+                            browser.loadUrl(jsUrlStart
+                                    + "getElementsByName('ssusername')[0].value = '"
+                                    + username + "'" + jsUrlEnd);
+                            browser.loadUrl(jsUrlStart
+                                    + "getElementsByName('sspassword')[0].value = '"
+                                    + password + "'" + jsUrlEnd);
+                            if (automaticallyLogin && (loginAttemptsLeft > 0)) {
+                                tryingToLogin = true;
+                                loginAttemptsLeft--;
+                                browser.loadUrl(jsUrlStart
+                                        + "forms['userForm'].submit()"
+                                        + jsUrlEnd);
+                            }
+                        } catch (Exception ex) {
+                            Log.e("Error : ",
+                                    "Error in onPageFinished() "
+                                            + ex.getMessage());
+                            ex.printStackTrace();
+                        }
+                    }
                 }
             }
         });  
-        loadSchoolsoft();
+
     }
 
     private void loadSchoolsoft() {
         try
         {     
-            //browser.loadUrl("file:///android_asset/globen.html");
-            browser.loadUrl("https://sms11.schoolsoft.se/globen/jsp/pda/Login.jsp");  
+            browser.loadUrl(SchoolSoftUrl);
         }
         catch(Exception ex)
         {
@@ -94,29 +137,38 @@ public class GlobenInfo extends Activity {
 
     @Override
     public void onResume() {
-      super.onResume();
+        super.onResume();
+        loadPrefs();
+        injectJavaScript();
         loadSchoolsoft();
     }
     
     @Override
     public void onPause() {
-      super.onPause();
+        super.onPause();
     }
 
     private boolean validConfig(String cfg) {
-        return (!(cfg.equals("UNKNOWN")) && !(cfg.equals("")));
+        return !(cfg.equals("UNKNOWN") || cfg.equals(""));
     }
 
     private void loadPrefs() {
-        SharedPreferences mySharedPreferences = PreferenceManager
+        SharedPreferences sharedPrefs = PreferenceManager
                 .getDefaultSharedPreferences(this);
 
-        username = mySharedPreferences.getString("username", "UNKNOWN");
-        password = mySharedPreferences.getString("password", "UNKNOWN");
-        // Log.e("username = ", username);
-        // Log.e("password = ", password);
-        if (!validConfig(username) || !validConfig(password)) {
+        String userPref = sharedPrefs.getString("username", "UNKNOWN");
+        String passPref = sharedPrefs.getString("password", "UNKNOWN");
+        if (!(validConfig(userPref) && validConfig(passPref))) {
             startPrefActivity();
+            return;
         }
+        automaticallyLogin = sharedPrefs.getBoolean("autoLogin", false);
+        if (!userPref.equals(username) || !passPref.equals(password)) {
+            if (automaticallyLogin) {
+                loginAttemptsLeft = 1;
+            }
+        }
+        username = userPref;
+        password = passPref;
     }
 }
--- src/com/orco/android/SetPreferenceActivity.java
+++ src/com/orco/android/SetPreferenceActivity.java
@@ -1,9 +1,15 @@
 package com.orco.android;
 
-import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public class SetPreferenceActivity extends Activity {
+public class SetPreferenceActivity extends PreferenceActivity {
+
+    protected PrefsFragment fragment = new PrefsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +17,42 @@ public class SetPreferenceActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new PrefsFragment()).commit();
+                .replace(android.R.id.content, fragment).commit();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(listener);
+
+        /*
+         * String str = PreferenceManager.getDefaultSharedPreferences(this)
+         * .getString("username", "HOPPLA"); ((EditTextPreference)
+         * fragment.findPreference("username")) .setSummary(str);
+         */
+
+        /*
+         * EditTextPreference username = (EditTextPreference) fragment
+         * .findPreference("username"); EditTextPreference password =
+         * (EditTextPreference) fragment .findPreference("password");
+         * username.setSummary
+         * (PreferenceManager.getDefaultSharedPreferences(this)
+         * .getString("username", "UNKNOWN"));
+         * password.setSummary(PreferenceManager
+         * .getDefaultSharedPreferences(this) .getString("password",
+         * "UNKNOWN"));
+         */
     }
 
+    // Use instance field for listener
+    // It will not be gc'd as long as this instance is kept referenced
+    OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences prefs,
+                String key) {
+            if (key.equals("username") || key.equals("password")) {
+                EditTextPreference value = (EditTextPreference) fragment
+                    .findPreference(key);
+                value.setSummary(prefs.getString(key, "UNKNOWN"));
+            }
+        }
+    };
+
+    //registerOnSharedPreferenceChangeListener(listener);
 }
